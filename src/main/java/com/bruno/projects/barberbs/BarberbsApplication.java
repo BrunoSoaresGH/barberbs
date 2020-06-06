e1fd1a83bbabe4368b169f14a5385a8169323fdf
Bruno Soares <brunosoaresbsv@gmail.com>
2020-06-06 11:55:26 -0300

    Project barberbs created

---
+++ src/main/java/com/bruno/projects/barberbs/BarberbsApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.projects.barberbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/projects/barberbs/BarberbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.projects.barberbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
