48ca717026cbe2e82da3d92f3f3ac9834ab998eb
Bruno Soares <brunosoaresbsv@gmail.com>
2020-06-08 19:49:29 -0300

    Creating instantiation operation

--- src/main/java/com/bruno/projects/barberbs/BarberbsApplication.java
+++ src/main/java/com/bruno/projects/barberbs/BarberbsApplication.java
@@ -1,13 +1,31 @@
 package com.bruno.projects.barberbs;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BarberbsApplication {
+import com.bruno.projects.barberbs.domain.Client;
+import com.bruno.projects.barberbs.repositories.ClientRepository;
 
+@SpringBootApplication
+public class BarberbsApplication implements CommandLineRunner{
+
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BarberbsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Client a = new Client(null, "Abel Braga", "88888888");
+		Client b = new Client(null, "Bruno Soares", "99999999");
+		
+		clientRepository.saveAll(Arrays.asList(a, b));
 	}
 
 }
